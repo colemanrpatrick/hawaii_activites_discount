106e22236ba57cb91b25c5e0afe07865b6f6e88b
patrick <patrickcoleman@Patricks-MacBook-Air-2.local>
2019-03-15 17:10:26 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// $(document).ready(function() {
+//   $(this).scrollTop(0);
+// })
+function scrollDown(element,num){
+  $('html,body').animate({
+    scrollTop: $('#' + element).offset().top - num
+  },'slow');
+}
+
+sIndex = 0;
+
+$('#next-checkpoint').on("click",function scrollNext(){
+  var el = $('.scroll-cont:eq('+sIndex+')')
+  $('html,body').animate({
+    scrollTop: el.offset().top
+  },'slow')
+  sIndex++;
+  if(sIndex>4){
+    sIndex = 0;
+  }
+  // console.log(sIndex,el.html());
+  $(this).html('<span>'+el.html()+'</span>')
+})
